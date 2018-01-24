244378c37805a84c74c51b415d4228e9e19d83d4
pimenvibritania <blackbrit13@gmail.com>
2018-01-24 08:07:36 +0700

    finishing auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Transformers\UserTransformer;
+use Auth;
 
 class AuthController extends Controller
 {
@@ -28,8 +29,28 @@ class AuthController extends Controller
     	$response =  fractal()
     		->item($user)
     		->transformWith(new UserTransformer)
+    		->addMeta([
+    			'token' => $user->api_token,
+    		])
     		->toArray();
 
     		return response()->json($response, 201);
+    }
+
+    public function login(Request $request, User $user)
+    {
+    	if (!Auth::attempt(['email' => $request->email,  'password' => $request->password ])) {
+    		return response()->json(['error'=> 'Your credential is wronng', 401]);
+    	}
+
+    	$user = $user->find(Auth::user()->id);
+
+    	return fractal()
+    		->item($user)
+    		->transformWith(new UserTransformer)
+    		->addMeta([
+    			'token' => $user->api_token,
+    		])
+    		->toArray();	
     }
 }
