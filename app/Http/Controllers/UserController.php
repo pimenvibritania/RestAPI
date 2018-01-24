1b4d315ae0f1b1381130fdba030f9258a09c100a
pimenvibritania <blackbrit13@gmail.com>
2018-01-24 09:41:57 +0700

    endpoint authentic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Transformers\UserTransformer;
-
+use Auth;
 
 
 class UserController extends Controller
@@ -22,5 +22,15 @@ class UserController extends Controller
     		->collection($users)
     		->transformWith(new UserTransformer)
     		->toArray();
+    }
+
+    public function profile(User $user)
+    {
+        $user = $user->find(Auth::user()->id);
+
+        return fractal()
+            ->item($user)
+            ->transformWith(new UserTransformer)
+            ->toArray();
     }
 }
