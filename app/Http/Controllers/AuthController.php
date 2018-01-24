f71a6ea7cdb28774006b2dc10f8503e28db001f3
pimenvibritania <blackbrit13@gmail.com>
2018-01-24 07:23:42 +0700

    after register fractal

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Transformers\UserTransformer;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request, User $user)
+    {
+
+    	$this->validate($request, [
+    		'name' 		=> 'required',
+    		'email'		=> 'required|email|Unique:users',
+    		'password'	=> 'required|min:6',
+    	]);
+
+    	$user = $user->create([
+    		'name'		=> $request->name,
+    		'email'		=> $request->email,
+    		'password'	=> bcrypt($request->password),
+    		'api_token'	=> bcrypt($request->email)
+    	]);
+
+    	$response =  fractal()
+    		->item($user)
+    		->transformWith(new UserTransformer)
+    		->toArray();
+
+    		return response()->json($response, 201);
+    }
+}
